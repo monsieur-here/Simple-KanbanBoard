83e144f0a793001c8845ec5abf25c53e7bd1d8fb
monsieur-here <53994789+monsieur-here@users.noreply.github.com>
2023-12-14 12:07:54 +0530

    Uploading the working of ticket add and remove

---
+++ index.js
@@ -0,0 +1,210 @@
+const addButton = document.querySelector(".add-btn")
+const removeButton = document.querySelector(".remove-btn")
+const modalContainer = document.querySelector(".modal-container")
+const ticketContainer = document.querySelector(".ticket-container")
+const ticketLocker = document.querySelector(".ticket-lock > i")
+
+const allContainers = document.querySelectorAll(".color")
+const textAreaContainer = document.querySelector(".textArea-container")
+const mainContainer = document.querySelector(".main-container")
+const allFilterContainers = document.querySelectorAll(".color")
+
+// To perform an HTML event we use addEventListener function hence in this case we need to open the task by addition
+
+let isTaskOpen = false
+let isDeleteModeActive = false
+let currentColor = "lightpink"
+let taskLock = false
+
+const lockedClass = "fa-lock"
+const unlockedClass = "fa-lock-open"
+
+const ticketsArray = []
+const colorsArray = ['lightpink', 'lightgreen', 'lightblue', 'black']
+
+function revisedRandId() {
+    return Math.random().toString(36).replace(/[^a-z]+/g, '').substring(2, 10)
+}
+
+addButton.addEventListener('click', event => {
+    if(!isTaskOpen){
+        //Open it
+        modalContainer.style.display = "flex"
+        // ticketContainer.style.opacity = '0.4'
+        isTaskOpen = true
+    }
+    else{
+        // Close it
+        modalContainer.style.display = "none"
+        isTaskOpen = false
+    }
+})
+
+removeButton.addEventListener('click', event => {
+    isDeleteModeActive = !isDeleteModeActive
+    if(isDeleteModeActive){
+        alert("Activated delete button")
+        removeButton.style.color = "red"
+    }
+    else{
+        alert("Deactivated delete button")
+        removeButton.style.color = "white"
+    }
+})
+
+// Add event listeners to the priority container so that the box we clicked on gets selected and the rest is removed
+
+allContainers.forEach((container) => {
+    container.addEventListener('click', ()=> {
+        allContainers.forEach((container) => {
+            container.classList.remove('active')
+        })
+        container.classList.add('active')
+        currentColor = container.classList[0]
+        // const textAreaValue = textAreaContainer.value
+        
+        // const id = 1354
+    })
+})
+
+modalContainer.addEventListener('keydown', event => {
+    if(event.key == 'Shift'){
+        //1. Get the value of current Priority which is already in the container
+        //2. Get the value of textArea and also add the id
+        const textAreaValue = textAreaContainer.value
+
+        const shortid = revisedRandId()
+
+        createTask(currentColor, textAreaValue, shortid)
+
+        // Reset the textArea value
+        textAreaContainer.value = ""
+
+        //Close the modal
+        modalContainer.style.display = "none"
+
+        // Reset the vairable which was tracking open or close status
+        isTaskOpen = false
+
+        // Reset currentColor
+        currentColor = "lightpink"
+    }
+})
+
+function createTask(taskType, taskValue, taskId){
+    
+    //1. Create a new element
+    const newDiv = document.createElement("div")
+    newDiv.classList.add("ticket-container")
+
+    //2. Modify the children of newly created div using innerHTML
+    //Generating the tasks dynamically using template literal ${}
+    newDiv.innerHTML = `<div class="ticket-color ${taskType}"></div>
+                        <div class="ticket-id">${taskId}</div>
+                        <div class="task-area">${taskValue}</div>
+                        <div class="ticket-lock">
+                            <i class="fa-solid fa-lock"></i>
+                        </div>`
+    
+    // Creating a ticket object to perform task filtering
+    const ticketObject  = {
+        id: taskId,
+        type: taskType,
+        value: taskValue
+    }
+
+    ticketsArray.push(ticketObject)
+    console.log(ticketsArray)
+    
+    handleRemoval(newDiv)
+    handleLock(newDiv, taskId)
+    handlePriorityChange(newDiv)
+    
+    //3. Append the children to main container
+    mainContainer.appendChild(newDiv)
+}
+
+function handleRemoval(div){
+    div.addEventListener('click', event => {
+        if(isDeleteModeActive)
+    // Two ways of removing or hiding an item
+    //1. Setting display to none
+    //2. Calling the remove function
+            div.remove()
+    })
+}
+
+function findTicketIdx(id) {
+    return ticketsArray.findIndex((ticket) => {
+        return ticket.id == id
+    })
+}
+
+
+function handleLock(divToBeLocked, ticketId) {
+    // Add an event listener to my lock
+    // Inspect the current class of the lock 
+    // Based on that, flip the class 
+    // Based on the class, also make the content of the div editable
+
+    const iconElement = divToBeLocked.querySelector(".ticket-lock > i")
+
+    const taskAreaElement = divToBeLocked.querySelector('.task-area')
+
+    iconElement.addEventListener('click', event => {
+
+        // Find out the index of my ticket 
+        const idx = findTicketIdx(ticketId)
+
+        // Figure out the class of my icon & make it flipped
+        
+
+        const classOfIcon = iconElement.classList[1]
+
+        if(classOfIcon == lockedClass) {
+            // Remove the class
+            iconElement.classList.remove(lockedClass)
+            // Add the unlock class
+            iconElement.classList.add(unlockedClass)
+            // Make the content editable
+            // taskAreaElement.focus()
+            taskAreaElement.setAttribute('contenteditable', 'true')
+
+            // console.log(ticketsArray)
+        } else {
+            // Remove the class
+            iconElement.classList.remove(unlockedClass)
+            // Add the unlock class
+            iconElement.classList.add(lockedClass)
+            // Make the content uneditable
+            taskAreaElement.setAttribute('contenteditable', 'false')
+
+            // Update my application state
+            ticketsArray[idx].value = taskAreaElement.innerText
+            // console.log({ ticketsArray })
+        }
+        
+    })
+}
+
+// Switching colors after the task is assigned
+function handlePriorityChange(colorSwitch){
+    const changeColor = colorSwitch.querySelector(".ticket-color")
+
+    changeColor.addEventListener('click', () =>{
+        const colorTag = changeColor.classList[1];
+        // console.log(colorTag)
+
+        const index = colorsArray.findIndex((elem) =>{
+            return elem == colorTag
+        })
+
+        // console.log(colorTag, index)
+        const newIdx = (index + 1) % colorsArray.length
+        changeColor.classList.remove(colorTag)
+        changeColor.classList.add(colorsArray[newIdx])
+
+    })
+}
+
+// Filtering colors
